675dc59a4d5ca1dba2689abf7bf6af013641b97b
yu <736575398@qq.com>
2017-08-26 19:49:44 +0800

    支付

--- app/src/main/java/hengai/com/shishuo/app/MyApplication.java
+++ app/src/main/java/hengai/com/shishuo/app/MyApplication.java
@@ -18,6 +18,7 @@ import com.scwang.smartrefresh.layout.impl.RefreshHeaderWrapper;
 import com.umeng.socialize.PlatformConfig;
 import com.umeng.socialize.UMShareAPI;
 
+import cn.beecloud.BeeCloud;
 import hengai.com.shishuo.R;
 
 /**
@@ -29,10 +30,16 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         UMShareAPI.get(this);
+      //支付
+        BeeCloud.setAppIdAndSecret("04250155-4651-42d1-917d-2f793f720806",
+                "811671b6-34d6-4db5-b020-484dcc8bf844");
 
+// 如果需要开启测试模式
+// BeeCloud.setSandbox(true);
+// BeeCloud.setAppIdAndSecret("appId", "testSecret");
     }
     {
-        PlatformConfig.setWeixin("wx5231d4d655cbf5c2", "875777926699ad7f9a0ad7675dfe2011");
+        PlatformConfig.setWeixin("wx5231d4d655cbf5c2","875777926699ad7f9a0ad7675dfe2011");
         PlatformConfig.setQQZone("1105807406", "IIwxLqdIAdZGG6R3");
         PlatformConfig.setSinaWeibo("2862859337", "06f988828740fee943633953dcf73ba3","http://liangshiba.com");
     }
